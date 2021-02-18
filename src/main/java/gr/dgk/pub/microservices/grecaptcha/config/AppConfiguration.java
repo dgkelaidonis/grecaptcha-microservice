7e760f561d849753e1d5c73cb04ece47cfad62ce
dgkelaidonis <kelaidonis@gmail.com>
2021-02-18 15:52:07 +0200

    improving configuration source.

--- src/main/java/gr/dgk/pub/microservices/grecaptcha/config/AppConfiguration.java
+++ src/main/java/gr/dgk/pub/microservices/grecaptcha/config/AppConfiguration.java
@@ -2,6 +2,7 @@ package gr.dgk.pub.microservices.grecaptcha.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * This class implements the configuration of the gReCaptcha microservice, for
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * their deployment into the context of a spring-boot application.
  */
 @Configuration
+@PropertySource("file:conf/grecaptcha-service.properties")
 @ComponentScan(basePackages = "gr.dgk.microservices.grecaptcha")
 public class AppConfiguration {
 }
